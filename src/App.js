5430d67a5c60577ba3e7fdef6061f23ac3ac16e0
Natalie Roode <nroode@gmail.com>
2019-09-09 19:24:04 -0700

    add mainApp component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+import MainApp from './components/mainApp';
 import Test from './components/test';
 
 function App() {
   return (
     <div className="App">
       Foodie Finds <i className="fa fa-500px"></i>
+      <MainApp />
       <Test />
     </div>
   );
---
+++ src/components/mainApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/main.scss';
+
+class mainApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+        };
+    }
+    
+    render() {
+      return(
+          <div>
+            modal, recipeList, header go here
+          </div>
+      );
+    }
+  }
+  
+  export default mainApp;
