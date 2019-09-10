e817752d9e044a174e63b4c1f28b03c4d715779a
Ken Truong <kenvantruong@gmail.com>
2019-09-09 22:02:56 -0700

    fixed header but needs feedback

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,26 @@ function Header() {
           <h6 className="receipe-text">RECEIPE FINDER</h6>
         </div>
         <div className='main-right-design'>
-          <h1>FoodieFinds</h1>
+          <div className="top-header">
+            <h1>FoodieFinds</h1>
+            <input type="text" className="searchBox" placeholder="Search millions of recipes ..."></input>
+          </div>
+          <div className="category-btn">
+            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+          </div>
         </div>
       </div>
       
 
 
 
-    <div className="platform">
+    {/* <div className="platform">
 
-    </div>
+    </div> */}
     </div>
   );
 }
