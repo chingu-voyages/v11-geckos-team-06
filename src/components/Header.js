600b30a91779b8e2f42addebceab8042ebca1662
Ken Truong <kenvantruong@gmail.com>
2019-09-26 20:04:50 -0700

    fixed Header.js to match Figma but still need responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,27 +6,40 @@ function Header() {
     <div className='overlay'>
       <div className='main-top'>
         <div className='main-left-design'>
-          <div className="adjust1"></div>
-          <div className="adjust2">
+          {/* <div className="adjust1"></div> */}
+          {/* <div className="adjust2"></div> */}
             <h6 className="receipe-text">RECEIPE FINDER</h6>
-          </div>
-          <div className="adjust3"></div>
+          
+          {/* <div className="adjust3"></div> */}
         </div>
         <div className='main-right-design'>
           <div className="top-header">
-            <div className="foodieFindsH1Div">
+            {/* <div className="foodieFindsH1Div"></div> */}
               <h1 className="foodieFindsH1">FoodieFinds</h1>
-            </div>
+            
             <div className="searchBoxDiv">
               <input type="text" className="searchBox" placeholder="Search millions of recipes ..."></input>
             </div>
+              
           </div>
-          <div className="category-btn">
-            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
-            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
-            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
-            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
-            <div><i class="fas fa-angle-double-down"></i><span>category 1</span></div>
+
+          {/* ------- Category Area ------  */}
+          <div className="category-main-div">
+            <div className="category-1 wrapthatflex">
+              <i class="fas fa-angle-double-down"></i><span>category 1</span>
+            </div>
+            <div className="category-2 wrapthatflex">
+              <i class="fas fa-angle-double-down"></i><span>category 1</span>
+            </div>
+            <div className="category-3 wrapthatflex">
+              <i class="fas fa-angle-double-down"></i><span>category 1</span>
+            </div>
+            <div className="category-4 wrapthatflex">
+              <i class="fas fa-angle-double-down"></i><span>category 1</span>
+            </div>
+            <div className="category-5 wrapthatflex">
+              <i class="fas fa-angle-double-down"></i><span>category 1</span>
+            </div>
           </div>
         </div>
       </div>
